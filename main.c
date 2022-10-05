dc1eff1236f9abafce72cfea055f70593732a360
Ilija <113606362+Ilija44@users.noreply.github.com>
2022-10-05 16:04:17 +0200

    add main

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+
+int main() {
+    printf("Hallo hier findet eine ganz einfache Rechnung statt\n" "11+4=?" "Ich hoffe das ich das richtig gerechnet habe und du etwas gelernt hast\n" );
+    int a = 11;
+            int  b = 4;
+
+                       printf("Das Ergebniss von 11+4 ist: %d", a + b);
+                                                                                                                                                                         printf("und jetzt NAAAA höör mal!!!!");
+    return a;
+}
